2269f7f28b3cb1f3688201a2029a1a1d68af5825
Majiyd <majeed@patricia.com.ng>
2020-03-17 13:36:08 +0100

    create basic get and post

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const contacts = [
+  { id: 1, name: 'jean' },
+  { id: 2, name: 'saul' },
+  { id: 3, name: 'dave' },
+];
 
 app.get('/', async (req, res) => {
-  res.send('hello world');
+  res.status(200).send('Welcome');
 });
 
-app.listen(3000, () => console.log(`Server running on localhost:${3000}`));
+app.get('/contacts', async (req, res) => {
+  res.status(200).send(contacts);
+});
+
+app.post('/contacts', async (req, res) => {
+  try {
+    if (!req.body?.name) {
+      res.status(400).send('Contact must have a name');
+      return;
+    }
+    const contact = {
+      id: contacts.length + 1,
+      name: req.body.name,
+    };
+    contacts.push(contact);
+    res.status(201).send(contact);
+  } catch (error) {
+    res.status(400).send(`Operation failed with error: ${error.message}`);
+  }
+});
+
+
+app.get('/contacts/:id', async (req, res) => {
+  const { id } = req.params;
+  const contact = contacts.find((c) => c.id === Number(id));
+  contact
+    ? res.status(200).send(contact)
+    : res.status(404).send(`Contact with id: ${id} not found`);
+});
+
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
