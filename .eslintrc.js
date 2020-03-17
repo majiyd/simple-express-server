2d02dabfc0afc5bd9cf9a138ded6f150feb25403
Majiyd <majeed@patricia.com.ng>
2020-03-17 10:45:38 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    'airbnb-base', 
+    "eslint:recommended",
+    "node"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "class-methods-use-this": 0,
+    "no-underscore-dangle": 0,
+    "global-require": 'off',
+    "camelcase": 0,
+    "quotes": ["error", "single"],
+    "no-use-before-define": ["error", { "functions": true, "classes": true, "variables": false }],
+    "import/prefer-default-export": 0,
+  }
+};
