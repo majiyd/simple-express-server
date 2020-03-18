7bafb059592bfa0040eadea13a7a5a6acb7e1f5c
Majiyd <majeed@patricia.com.ng>
2020-03-18 23:29:30 +0100

    experimenting with multiple routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/', router);
 app.use(async (req, res) => {
@@ -14,5 +16,5 @@ app.use(async (req, res) => {
 });
 
 // PORT
-const port = process.env.PORT || 2100;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on localhost:${port}`));
