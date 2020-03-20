e5fa48cb5927c2c5d72d87530d52b9a1a7683c14
Majiyd <majeed@patricia.com.ng>
2020-03-20 02:44:44 +0100

    get one contact from model

--- controllers/contacts/get.js
+++ controllers/contacts/get.js
@@ -1,29 +1,65 @@
-import db from '../../config/database';
+import Joi from '@hapi/joi';
 import { Contact } from '../../models/contacts';
-import contacts from '../../data';
 
 const getOne = async (req, res) => {
-  const { id } = req.params;
-  const contact = contacts.find((c) => c.id === Number(id));
-  contact
-    ? res.status(200).send(contact)
-    : res.status(404).send(`Contact with id: ${id} not found`);
-};
-
-const getAll = async (req, res) => {
-  Contact.findAll()
-    .then((c) => {
+  const schema = Joi.object({
+    id: Joi.number().required(),
+  });
+  const { error, value } = schema.validate(req.params);
+  if (error) {
+    res.status(400).json({
+      status: 400,
+      message: 'Error fetching contact',
+      errors: [error.message],
+    });
+    return;
+  }
+  const { id } = value;
+  Contact.findAll({
+    where: {
+      id,
+    },
+  })
+    .then((contact) => {
+      if (Array.isArray(contact) && contact.length === 0) {
+        res.status(404).json({
+          status: 404,
+          message: 'Error fetching contact',
+          errors: [`Contact with id: ${id} not found`],
+        });
+        return;
+      }
       res.status(200).json({
         status: 200,
-        message: 'Contacts fetched successfully',
-        data: c,
+        message: 'Contact fetched successfully',
+        data: contact,
       });
-    }).catch((err) => {
-      res.status(400).json({
-        status: 400,
-        message: err.message,
+    })
+    .catch((err) => {
+      res.status(404).json({
+        status: 404,
+        message: `Contact with id: ${id} not found`,
         errors: [err.message],
       });
     });
 };
+
+const getAll = async (req, res) => {
+  Contact.findAll()
+    .then((contacts) => {
+      res.status(200).json({
+        status: 200,
+        message: 'Contacts fetched successfully',
+        data: contacts,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        status: 400,
+        message: 'Error fetching contacts',
+        errors: [err.message],
+      });
+    });
+};
+
 export { getOne, getAll };
