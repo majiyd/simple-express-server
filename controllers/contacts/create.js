b1529c55dcc725ccb02b74e643dff176bac10a49
Majiyd <majeed@patricia.com.ng>
2020-03-20 03:13:22 +0100

    add create contact

--- controllers/contacts/create.js
+++ controllers/contacts/create.js
@@ -1,6 +1,6 @@
 
 import Joi from '@hapi/joi';
-import contacts from '../../data';
+import { Contact } from '../../models/contacts';
 
 
 export default async (req, res) => {
@@ -18,13 +18,29 @@ export default async (req, res) => {
       });
       return;
     }
-    const contact = {
-      id: contacts.length + 1,
-      name: value.name,
-    };
-    contacts.push(contact);
-    res.status(201).send(contact);
+
+    const { name } = value;
+
+    Contact.create({ name })
+      .then((contact) => {
+        res.status(201).json({
+          status: 201,
+          message: 'Contact created successfully',
+          data: contact,
+        });
+      })
+      .catch((err) => {
+        res.status(400).json({
+          status: 400,
+          message: 'Error creating contact',
+          errors: [err.message],
+        });
+      });
   } catch (error) {
-    res.status(400).send(`Operation failed with error: ${error.message}`);
+    res.status(400).json({
+      status: 400,
+      message: `Operation failed with error: ${error.message}`,
+      errors: [error.message],
+    });
   }
 };
--- models/contacts.js
+++ models/contacts.js
@@ -2,18 +2,8 @@ import Sequelize from 'sequelize';
 import db from '../config/database';
 
 const Contact = db.define('contacts', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-  },
   name: {
     type: Sequelize.STRING,
-  },
-  createdAt: {
-    type: Sequelize.TIME,
-  },
-  updatedAt: {
-    type: Sequelize.TIME,
   },
 });
 
