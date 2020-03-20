5312e0462057652f760e77c016e21b36e892da5d
Majiyd <majeed@patricia.com.ng>
2020-03-20 11:31:01 +0100

    fix bug in GenerateResponse

--- utils/generateResponse.js
+++ utils/generateResponse.js
@@ -1,41 +1,34 @@
 export default class GenerateResponse {
-  constructor(res, message, data, status) {
-    this.res = res;
-    this.message = message;
-    this.data = data;
-    this.status = status;
-  }
-
-  static async get() {
-    return this.res.status(200).json({
+  static get(res, message, data) {
+    return res.status(200).json({
       status: 200,
-      message: this.message,
-      data: this.data,
+      message,
+      data,
     });
   }
 
-  static async create() {
-    return this.res.status(201).json({
+  static async create(res, message, data) {
+    return res.status(201).json({
       status: 201,
-      message: this.message,
-      data: this.data,
+      message,
+      data,
     });
   }
 
-  static async notFound() {
-    return this.res.status(404).json({
+  static async notFound(res, message, data) {
+    return res.status(404).json({
       status: 404,
-      message: this.message,
-      errors: [this.data],
+      message,
+      errors: [data],
     });
   }
 
-  static async error() {
-    const status = this.status ? this.status : 400;
-    return this.res.status(status).json({
-      status,
-      message: this.message,
-      errors: [this.data],
+  static async error(res, message, data, status) {
+    const _status = status || 400;
+    return res.status(_status).json({
+      status: _status,
+      message,
+      errors: [data],
     });
   }
 }
