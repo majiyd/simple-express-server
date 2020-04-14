034456233f483861dfb0f04ab7669cb8932c748f
Majiyd <majeed@patricia.com.ng>
2020-04-14 21:45:18 +0100

    Implement phoneCalls/create with validators

--- controllers/phoneCalls/create.js
+++ controllers/phoneCalls/create.js
@@ -1,16 +1,32 @@
 import Joi from '@hapi/joi';
-import { PhoneCall } from '../../models';
+import { PhoneCall, Contact } from '../../models';
 import Response from '../../utils/response';
 
 export default async (req, res) => {
   try {
-    const { type, callerID } = req.body;
+    const schema = Joi.object({
+      type: Joi.string().valid('received', 'outgoing').required(),
+      callerID: Joi.number().required(),
+    });
+
+    const { error, value } = schema.validate(req.body);
+    if (error) return Response.error(res, 'Validation Error', [error.message]);
+
+    const { type, callerID } = value;
+
+    const isValidContact = await Contact.findOne({
+      where: {
+        id: callerID,
+      },
+    });
+
+    if (!isValidContact) return Response.error(res, 'Validation Error', [`Contact with id: ${callerID} doesn't exist`]);
+
     PhoneCall.create({ type, callerID })
       .then((phoneCall) => {
         Response.create(res, 'Phone call added successfully', phoneCall);
       })
       .catch((err) => {
-        console.log('err', err);
         Response.error(res, 'Failed to create phone call', [err.message]);
       });
   } catch (error) {
